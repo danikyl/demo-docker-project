fc8b796ed44b8514055c913397098dc080c92926
Daniel <daniel.m.fucci@gmail.com>
2021-06-22 12:14:05 -0300

    Adding Spring Boot structure with due dependencies

---
+++ aplication/src/test/java/com/danielsoftware/aplication/AplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsoftware.aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

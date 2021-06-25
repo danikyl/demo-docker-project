d0b6af5d8de2833b020ad83a786388aee304cecb
Daniel <daniel.m.fucci@gmail.com>
2021-06-24 21:38:12 -0300

    Disabling CORS and removing unnecessary dependencies

--- src/main/java/com/danielsoftware/aplication/controller/SubscriptionController.java
+++ src/main/java/com/danielsoftware/aplication/controller/SubscriptionController.java
@@ -1,2 +1,16 @@
-package com.danielsoftware.aplication.controller;public class SubscriptionController {
+package com.danielsoftware.aplication.controller;
+
+import com.danielsoftware.aplication.domain.dto.SubscriptionNotificationRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/subscription")
+public class SubscriptionController {
+
+    @PostMapping(path = "/update")
+    public void updateSubscription(SubscriptionNotificationRequest subscriptionNotificationRequest) {
+        System.out.println(subscriptionNotificationRequest.toString());
+    }
 }
--- src/main/java/com/danielsoftware/aplication/domain/dto/SubscriptionNotificationRequest.java
+++ src/main/java/com/danielsoftware/aplication/domain/dto/SubscriptionNotificationRequest.java
@@ -1,2 +1,19 @@
-package com.danielsoftware.aplication.domain;public class SubscriptionDomain {
+package com.danielsoftware.aplication.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class SubscriptionNotificationRequest {
+    @JsonProperty("notification_type")
+    private String notificationType;
+
+    @JsonProperty("subscription")
+    private String subscriptionId;
 }
